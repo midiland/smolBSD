fe17eb42fdc0d5e5b68098f64797634688a4eb6b
Emile 'iMil' Heitor <imil@NetBSD.org>
2023-02-01 18:50:31 +0100

    feat: kernel as a parameter

--- kstrip.sh
+++ kstrip.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+[ $# -lt 1 ] && exit 1
+
+kern=$1
+
 keep="mainbus cpu acpicpu ioapic pci isa pcdisplay wsdisplay com virtio ld vioif"
 
-gdb -n netbsd --batch -x drvdig.gdb -ex "loop_cfdata" | \
+gdb -n $kern --batch -x drvdig.gdb -ex "loop_cfdata" | \
 while read addr drv x
 do
 	found=0
@@ -14,5 +18,5 @@ do
 	[ $found -eq 1 ] && echo "not removing $drv" && continue
 
 	echo "removing $drv"
-	gdb -n netbsd --write --batch -x drvdig.gdb -ex "fstate 3 0x$addr"
+	gdb -n $kern --write --batch -x drvdig.gdb -ex "fstate 3 0x$addr"
 done
