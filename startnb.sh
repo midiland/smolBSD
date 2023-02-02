350dd483be945ed9af0dd3f83cd4850a8372b53d
Emile 'iMil' Heitor <imil@NetBSD.org>
2023-02-02 11:56:41 +0100

    fix: q35 not needed

--- startnb.sh
+++ startnb.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 sudo kvm -kernel $1 -append "console=com root=ld0a" \
-	-machine type=q35 \
 	-serial stdio -display none \
 	-drive file=ext2root.img,if=virtio \
 	-netdev type=tap,id=net0 -device virtio-net-pci,netdev=net0
