17a840546dfc8d337d71e06cf5b080bd6f48cf1b
Emile 'iMil' Heitor <imil@NetBSD.org>
2023-10-24 08:14:47 +0200

    fix: classier img= thanks lea ;) + added hostfwd netdev

--- startnb.sh
+++ startnb.sh
@@ -2,7 +2,7 @@
 
 kernel=$1
 
-[ -n "$2" ] && img=$2 || img=root.img
+img=${2:-"root.img"}
 
 [ -n "$3" ] && drive2="-drive file=${3},if=virtio"
 
@@ -10,4 +10,5 @@ qemu-system-x86_64 -enable-kvm -m 256 \
 	-kernel $kernel -append "console=com root=ld0a" \
 	-serial mon:stdio -display none \
 	-drive file=${img},if=virtio $drive2 \
-	-netdev type=tap,id=net0 -device virtio-net-pci,netdev=net0
+	-netdev type=tap,id=net0 -device virtio-net-pci,netdev=net0 \
+	-netdev user,id=net1,hostfwd=tcp::8080-:80 -device virtio-net,netdev=net1
