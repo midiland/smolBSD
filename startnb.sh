40210e2c40a68fdee6268a6843181f83b2bc24c7
Emile 'iMil' Heitor <imil@NetBSD.org>
2024-06-25 21:08:01 +0200

    fix: add root disk

--- startnb.sh
+++ startnb.sh
@@ -4,12 +4,13 @@ usage()
 {
 	cat 1>&2 << _USAGE_
 Usage:	${0##*/} -k kernel -i image [-a kernel parameters] [-m memory in MB]
-	[-d drive2] [-p port] [-w path]
+	[-r root disk] [-d drive2] [-p port] [-w path]
 
 	Boot a microvm
 	-k kernel	kernel to boot on
 	-a parameters	append kernel parameters
 	-m memory	memory in MB
+	-r root disk	root disk to boot on
 	-i image	image to use as root filesystem
 	-d drive2	second drive to pass to image
 	-p ports	[tcp|udp]:[hostaddr]:hostport-[guestaddr]:guestport
@@ -22,15 +23,16 @@ _USAGE_
 
 [ $# -lt 4 ] && usage
 
-options="k:a:p:i:m:d:p:w:h"
+options="k:a:p:i:m:r:d:p:w:h"
 
 while getopts "$options" opt
 do
 	case $opt in
 	k) kernel="$OPTARG";;
-	a) append="$OPTARG";;
 	i) img="$OPTARG";;
+	a) append="$OPTARG";;
 	m) mem="$OPTARG";;
+	r) root="$OPTARG";;
 	d) drive2="\
 		-device virtio-blk-device,drive=smolhd1 \
 		-drive if=none,file=${OPTARG},format=raw,id=smolhd1";;
@@ -70,12 +72,12 @@ case $MACHINE in
 x86_64|i386)
 	mflags="-M microvm,x-option-roms=off,rtc=on,acpi=off,pic=off${ACCEL}"
 	cpuflags="-cpu host,+invtsc"
-	root="ld0a"
+	root=${root:-"ld0a"}
 	;;
 aarch64)
 	mflags="-M virt${ACCEL}"
 	cpuflags="-cpu host"
-	root="ld4a"
+	root=${root:-"ld4a"}
 	extra="-device virtio-rng-pci"
 	;;
 *)
