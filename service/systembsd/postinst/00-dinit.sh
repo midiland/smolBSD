a9262d4be13e746bd402ca86c244769340d30c0e
Emile 'iMil' Heitor <imil@NetBSD.org>
2025-01-29 17:11:29 +0100

    feat: use kqueue(2)

--- service/systembsd/postinst/00-dinit.sh
+++ service/systembsd/postinst/00-dinit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # dinit is not yet part of a pkgsrc release
-curl -L -s -o packages/dinit.tgz https://imil.net/NetBSD/dinit-0.19.3nb1.tgz
+curl -L -s -o packages/dinit.tgz https://imil.net/NetBSD/dinit-0.19.3nb2.tgz
 
 mkdir -p usr/pkg
 $TAR zxvfp packages/dinit.tgz --exclude='+*' -C usr/pkg
