0f39f3ea31aaeec35d63a2dac4cd3c09265f5a9e
Emile 'iMil' Heitor <imil@NetBSD.org>
2025-09-07 11:43:11 +0200

    feat: add nitroBSD!

---
+++ service/nitro/postinst/00-nitro.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# bare minimum
+mknod -m 600 dev/console c 0 0
+mknod -m 600 dev/constty c 0 1
+mknod -m 666 dev/tty c 1 0
+mknod -m 666 dev/null c 2 2
+mknod -m 666 dev/stdin c 22 0
+mknod -m 666 dev/stdout c 22 1
+mknod -m 666 dev/stderr c 22 2
+
+mkdir -p packages
+VERSION=0.3.0.0.20250907
+curl -L -s -o packages/nitro.tgz https://imil.net/NetBSD/nitro-${VERSION}.tgz?$RANDOM
+
+PREFIX=usr/pkg
+mkdir -p ${PREFIX}
+$TAR zxvfp packages/nitro.tgz --exclude='+*' -C ${PREFIX}
+
+mv ${PREFIX}/sbin/nitro sbin/init
+for d in var/run/nitro ${PREFIX}/etc/nitro/SYS ${PREFIX}/etc/nitro/getty-0
+do
+	mkdir -p $d
+done
+
+ln -sf /var/run/nitro/nitro.sock ${PREFIX}/etc/nitro.sock
+
+cat >${PREFIX}/etc/nitro/getty-0/run<<EOF
+#!/bin/sh
+exec /usr/libexec/getty Pc constty
+EOF
+cat >${PREFIX}/etc/nitro/getty-0/finish<<EOF
+#!/bin/sh
+exit 0
+EOF
+for s in run finish
+do
+	chmod +x ${PREFIX}/etc/nitro/getty-0/$s
+done
+
+cat >${PREFIX}/etc/nitro/SYS/finish<<EOF
+#!/bin/sh
+sleep 10
+echo FINISH
+poweroff
+EOF
+
+chmod +x ${PREFIX}/etc/nitro/SYS/finish
